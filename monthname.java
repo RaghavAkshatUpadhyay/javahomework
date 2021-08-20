25ae695ca45081278ee835fe579875b8d55a9183
zuccionoob <raghavakshat7411@gmail.com>
2021-08-21 01:34:57 +0530

    ques2

---
+++ monthname.java
@@ -0,0 +1,8 @@
+class month{
+ public static void main(String[] args) {
+ 
+ int sum=Integer.parseInt(args[0]);
+ String r[]={" ","jan","Feb","March","April","May","June","July","August","Sept","Oct","Nov","Dec"};
+System.out.println(r[sum]);
+ }
+}
