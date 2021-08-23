d21cb19d84b13c3aac5150c57ef1b2c793890881
zuccionoob <78753926+zuccionoob@users.noreply.github.com>
2021-08-23 13:57:54 +0530

    Update month.java

--- month.java
+++ month.java
@@ -1,8 +1,12 @@
 class month{
  public static void main(String[] args) {
  
- int sum=Integer.parseInt(args[0])
- String r={" ","jan","Feb","March","April","May","June","July","August","Sept","Oct","Nov","Dec"};
-System.out.println(r[sum]);
+ int sum=0;
+ for(int i=41;i<250;i++){
+  if(i%5==0){
+ sum=sum+i;
+ }
+  }
+System.out.println(sum);
  }
 }
